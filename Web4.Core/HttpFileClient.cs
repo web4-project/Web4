30d1b7d141d8dc546538ace4c89de89048d17d8f
Tim Miller <innerlogic4321@gmail.com>
2022-05-08 15:36:20 -0400

    Add default headers for HTTP Client

--- Web4.Core/HttpFileClient.cs
+++ Web4.Core/HttpFileClient.cs
@@ -12,13 +12,18 @@ namespace Web4.Core
     /// </summary>
     public class HttpFileClient : IHttpFileClient
     {
+        private const string Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+        private const string Language = "ja,en-US;q=0.8,en;q=0.6";
+        private const string Encoding = "gzip, deflate, sdch";
+        private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2593.0 Safari/537.36";
+
         private HttpClient httpClient;
         private ILogger<HttpFileClient>? logger;
         private string defaultDownloadPath;
 
         public HttpFileClient(HttpClient? client = default, ILogger<HttpFileClient>? logger = default, string? defaultDownloadPath = default)
         {
-            this.httpClient = client ?? new HttpClient();
+            this.httpClient = client ?? this.GenerateClient();
             this.logger = logger ?? default;
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
             this.defaultDownloadPath = Path.Combine(path, defaultDownloadPath ?? "Downloads");
@@ -59,6 +64,17 @@ namespace Web4.Core
         public Task<string> UploadFile(string filePath, MultipartFormDataContent? dataForm = null)
         {
             throw new NotImplementedException();
+        }
+
+        private HttpClient GenerateClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", Accept);
+            client.DefaultRequestHeaders.Add("Accept-Language", Language);
+            client.DefaultRequestHeaders.Add("Accept-Encoding", Encoding);
+            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+            client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            return client;
         }
     }
 }
